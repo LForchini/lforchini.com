b13544d39159b60d64789e72626db10f40971d61
LForchini <leonardo.forchini@gmail.com>
2022-08-03 19:09:37 +0100

    Added basic routes

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,8 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ProjectInfoPayload {
+    pub name: String,
+    pub link: Option<String>,
+    pub desc: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+pub struct ProjectInfo {
+    pub id: u32,
+    pub name: String,
+    pub link: Option<String>,
+    pub desc: String,
 }
