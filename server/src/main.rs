009f3dedd8e666eff5e7fa8b854b39db7c3d01bc
LForchini <leonardo.forchini@gmail.com>
2022-08-03 11:23:48 +0100

    Added basic structure of server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,23 +1,12 @@
+use axum::{routing::get, Router};
 use clap::Parser;
+use std::net::SocketAddr;
 
 #[derive(Debug, Parser)]
 struct Args {
-    /// Flag for whether running in production mode or not.
-    /// This will enable HTTPS features.
-    #[clap(long)]
-    production: bool,
-
-    /// Path to private key
-    #[clap(short, long, value_parser, value_name = "FILE")]
-    key_filename: Option<String>,
-
-    /// Path to HTTPS certificate
-    #[clap(short, long, value_parser, value_name = "FILE")]
-    cert_filename: Option<String>,
-
     /// Port to run on
-    #[clap(short, long, value_parser)]
-    port: Option<u16>,
+    #[clap(short, long, value_parser, default_value_t = 8088)]
+    port: u16,
 }
 
 #[tokio::main]
@@ -27,21 +16,21 @@ async fn main() {
     log::info!("Initialising server");
 
     log::debug!("Reading arguments");
-    let cli_args = Args::parse();
+    let args = Args::parse();
 
-    if cli_args.production {
-        log::debug!("Running in production mode");
-        if cli_args.key_filename == None || cli_args.cert_filename == None {
-            if cli_args.key_filename == None {
-                log::error!("Key file was not specified");
-            }
-            if cli_args.cert_filename == None {
-                log::error!("Cert file was not specified");
-            }
+    log::debug!("Creating routes");
+    let app = Router::new().route("/", get(root));
 
-            panic!("Cannot run in production mode without specifying HTTPS files");
-        }
-    } else {
-        log::debug!("Running in developer mode");
-    }
+    log::debug!("Running on 127.0.0.1:{:#?}", args.port);
+    let addr = SocketAddr::from(([127, 0, 0, 1], args.port));
+
+    log::info!("Starting Server");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
