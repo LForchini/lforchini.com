fa5f964e90da80d3d8b9fa7159656184b852a3c5
Leonardo Forchini <git@lforchini.com>
2022-10-13 15:17:42 +0100

    Setup for rewrite

--- shared/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Clone, Debug, Default)]
-pub struct Project {
-    pub name: String,
-    pub link: Option<String>,
-    pub desc: String,
-}
