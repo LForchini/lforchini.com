7510ffe97450df70139b63cc66848e438e96f8fd
Leonardo Forchini <git@lforchini.com>
2022-11-18 14:46:12 +0000

    Add mastodon link

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,8 +1,13 @@
+use crate::components::mastodon::Mastodon;
 use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <h1>{ "Welcome to my web site :)" }</h1>
+        <>
+             <h1>{ "Welcome to my web site :)" }</h1>
+
+             <Mastodon />
+        </>
     }
 }
---
+++ client/src/components/mastodon.rs
@@ -0,0 +1,8 @@
+use yew::prelude::*;
+
+#[function_component(Mastodon)]
+pub fn mastodon() -> Html {
+    html! {
+        <a rel="me" href="https://fosstodon.org/@LForchini"> {"Mastodon"}</a>
+    }
+}
